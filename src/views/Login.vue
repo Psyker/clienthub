7a915dd375e621e1d4f09139492622b4d614b8df
Psyker <bourgoin.theo1@gmail.com>
2019-02-08 17:23:34 +0100

    Create user login mutation and view

---
+++ src/views/Login.vue
@@ -0,0 +1,24 @@
+<template>
+    <div>
+        <vue-headful :title="ClientHub - Connexion"/>
+        <ApolloMutation :mutation="require('../graphql/mutations/LoginUserMutation.graphql')"
+                        :variables="{ input: { username, password } }"
+                        @done="onSuccessLogin">
+            <template slot-scope="{ mutate, loading, gqlError }">
+                <form action="" method="post" @submit.prevent="mutate" class="login-form">
+
+                </form>
+            </template>
+        </ApolloMutation>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: "Login"
+    }
+</script>
+
+<style scoped>
+
+</style>
